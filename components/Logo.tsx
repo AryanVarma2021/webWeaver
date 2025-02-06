c80c49e03b0bf551b1b6bfc6b1bc4304dd5c8409
AryanVarma2021 <varmaaryan11@gmail.com>
2025-02-06 20:50:27 +0530

    side bar is ready

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils';
+import { SquareDashedMousePointer } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react'
+
+const Logo = ({fontSize = "text-2xl", iconSize = 20} : {fontSize?:string ; iconSize?:number}) => {
+  return (
+    <Link href="" className={cn("text-2xl flex items-center gap-2", fontSize)}
+        
+    >
+        <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2 ">
+            <SquareDashedMousePointer size={iconSize} className='stroke-white'/> 
+        </div>
+        <div className="">
+            <span className='bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent'>
+                Web
+            </span>
+            <span></span>
+        </div>
+    </Link>
+  )
+}
+
+export default Logo
